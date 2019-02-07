8c9208ed6427e67db91637e6b411f9312e3e9c44
Yannik Hackbarth <haggiderzweite@googlemail.com>
2019-02-07 22:41:13 +0100

    Make parameter lowercase

--- sdORM/Session/Exceptions/DataBaseSessionFactoryNotInitializedException.cs
+++ sdORM/Session/Exceptions/DataBaseSessionFactoryNotInitializedException.cs
@@ -5,8 +5,8 @@ namespace sdORM.Session.Exceptions
     [Serializable]
     public class DatabaseSessionFactoryNotInitializedException : Exception
     {
-        public DatabaseSessionFactoryNotInitializedException(Type DatabaseSessionFactoryType)
-            : base($"Your {DatabaseSessionFactoryType} has not been initialized. Please call Initialize() or InitializeAsync() before creating sessions.")
+        public DatabaseSessionFactoryNotInitializedException(Type databaseSessionFactoryType)
+            : base($"Your {databaseSessionFactoryType} has not been initialized. Please call Initialize() or InitializeAsync() before creating sessions.")
         {
             
         }
